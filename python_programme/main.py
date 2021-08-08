a3289a31fbdf9893aca0c4615423726feaa4d0f9
wyyadd <wyyadd@gmail.com>
2021-08-08 10:43:03 +0800

    daka

--- python_programme/main.py
+++ python_programme/main.py
@@ -1,104 +1,119 @@
-import requests
-from fake_useragent import UserAgent
-from bs4 import BeautifulSoup
-import math, sys, time, random, collections
-import numpy as np
-import pandas as pd
+import time
+from threading import Timer
+import random
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-""" 
-A python script to calculate Normalized Google Distance 
-The Normalized Google Distance (NGD) is a semantic similarity measure, 
-calculated based on the number of hits returned by Google for a set of 
-keywords. If keywords have many pages in common relative to their respective, 
-independent frequencies, then these keywords are thought to be semantically 
-similar. 
-If two search terms w1 and w2 never occur together on the same web 
-page, but do occur separately, the NGD between them is infinite. 
-If both terms always (and only) occur together, their NGD is zero.
-"""
+# 加启动配置 禁用日志log
+chrome_opt = Options()
+# –no - sandbox 参数是让root权限下跑
+chrome_opt.add_argument('–no-sandbox')
+chrome_opt.add_argument('--start-maximized')  # 最大化
+chrome_opt.add_argument('--incognito')  # 无痕隐身模式
+chrome_opt.add_argument("disable-cache")  # 禁用缓存
+chrome_opt.add_argument('log-level=3')
+chrome_opt.add_argument('disable-infobars')
+chrome_opt.add_argument('--headless')
 
-def NGD(w1, w2):
-  """
-  Returns the Normalized Google Distance between two queries.
-  Params:
-   w1 (str): word 1
-   w2 (str): word 2
-  Returns:
-   NGD (float)
-  """
-  N = 25270000000.0 # Number of results for "the", proxy for total pages
-  N = math.log(N,2) 
-  if w1 != w2:
-    f_w1 = math.log(number_of_results(w1),2)
-    f_w2 = math.log(number_of_results(w2),2)
-    f_w1_w2 = math.log(number_of_results(w1+" "+w2),2)
-    NGD = (max(f_w1,f_w2) - f_w1_w2) / (N - min(f_w1,f_w2))
-    return NGD
-  else: 
-    return 0
- 
-def calculate_NGD(w1, w2, n_retries=10):
-  """ 
-  Attempt to calculate NGD. 
-  We will attempt to calculate NGD, trying `n_retries`. (Sometimes Google throws
-  captcha pages. But we will just wait and try again). Iff all attempts fail, 
-  then we'll return NaN for this pairwise comparison. 
-  Params: 
-    w1 (str): word 1 
-    w2 (str): word 2
-    retries (int): Number of attempts to retry before returning NaN 
-  Returns:
-    if succesful:
-      returns NGD
-    if not succesful:
-      returns np.NaN
-  """
 
-  for attempt in range(n_retries):
-    try:
-      return NGD(w1, w2)
-    except Exception as e:
-      print("Trying again...")
-      print(e)
-  else: 
-    print("Sorry. We tried and failed. Returning NaN.")
-    return np.NaN
+class SeuDaKa(object):
+    def __init__(self):
+        # 打卡网址
+        self.login_url = "https://newids.seu.edu.cn/authserver/login?service=http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/*default/index.do"
+        # 登出网址
+        self.logout_url = "https://newids.seu.edu.cn/authserver/index.do"
+        self.browser = webdriver.Chrome()
 
-def pairwise_NGD(element_list, retries=10):
-  """Compute pairwise NGD for a list of terms"""
-  distance_matrix = collections.defaultdict(dict) # init a nested dict
-  for i in element_list:
-    sleep(5, 10)
-    for j in element_list:
-      try: # See if we already calculated NGD(j, i)
-        print(i, j)
-        distance_matrix[i][j] = distance_matrix[j][i]
-      except KeyError: # If not, calculate NGD(i, j)
-        distance_matrix[i][j] = calculate_NGD(i, j, retries)
-  return distance_matrix
+    def login(self, username, password):
+        self.browser.get(self.login_url)
+        # 等待用户名和密码框加载完成
+        username_field = WebDriverWait(self.browser, 15, 0.5).until(
+            EC.presence_of_element_located((By.ID, 'username')))
+        password_field = WebDriverWait(self.browser, 15, 0.5).until(
+            EC.presence_of_element_located((By.ID, 'password')))
+        username_field.clear()
+        password_field.clear()
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+        login_button = self.browser.find_element_by_class_name('auth_login_btn')
+        login_button.submit()
+        # 等待打卡页面加载完成（这里是看"基本信息上报"是否加载完成）
+        self.wait_by_xpath('//*[@id="app"]/div/div[1]/button')
 
-def pairwise_NGD_to_df(distances):
-  """Returns a dataframe of pairwise NGD calculations"""
-  df_data = {} 
-  for i in distances:
-    df_data[i] = [distances[i][j] for j in distances]
-  df = pd.DataFrame(df_data)
-  df.index = distances
-  return df 
+    def wait_by_xpath(self, loc, timeout=15):
+        return WebDriverWait(self.browser, timeout, 0.5).until(EC.presence_of_element_located((By.XPATH, loc)))
 
-def number_of_results(text):
-  """Returns the number of Google results for a given query."""
-  headers = {'User-Agent': UserAgent().firefox}
-  sleep(5, 10)
-  r = requests.get("https://www.google.com/search?q={}".format(text.replace(" ","+")), headers=headers)
-  soup = BeautifulSoup(r.text, "lxml") # Get text response
-  res = soup.find('div', {'id': 'result-stats'}) # Find result string 
-  return int(res.text.replace(",", "").split()[1]) # Return result int
+    def logout(self):
+        # 坑人的 SEU 会记录登录的ip和浏览器导致即使关闭标签页也无法退出登录，为下一位同学的登录带来问题
+        self.browser.get(self.logout_url)  # 这是登录认证界面
+        logout_button = WebDriverWait(self.browser, 15, 0.5).until(
+            EC.presence_of_element_located((By.ID, 'logout')))
+        if logout_button:
+            logout_button.click()
+        return self.browser
 
-def sleep(alpha, beta):
-  """Sleep for an amount of time in range(alpha, beta)"""
-  rand = random.Random()
-  time.sleep(rand.uniform(alpha, beta))
+    def report(self):
+        # 检查是否存在新增按钮
+        time.sleep(1)
+        buttons = self.browser.find_elements_by_tag_name('button')
+        daily_done = True
+        for button in buttons:
+            if button.get_attribute("textContent").find("新增") >= 0:
+                new_button = button
+                daily_done = False
+                break
+
+        # 检查是否存在退出按钮
+        exit_button = self.browser.find_element_by_xpath(
+            '//*[@id="app"]/div/div[1]/button')
+
+        if daily_done is True and exit_button:  # 今日已完成打卡
+            print("今日已完成打卡任务")
+
+        elif daily_done is False and exit_button:  # 今日未完成打卡
+            # 点击报平安
+            new_button.click()
+            time.sleep(2)
+            # 输入当天晨检体温, 36.5-37之间的随机值
+            input_filed = self.wait_by_xpath(
+                '//*[@id="app"]/div/div/div[2]/div/div[4]/div/div[2]/div[1]/div/a/div[2]/div[2]/input')
+            input_filed.click()
+            input_filed.send_keys(str(random.randint(365, 370) / 10.0))
+            submit_button = self.browser.find_element_by_xpath(
+                '//*[@id="app"]/div/div/div[3]/button')
+            submit_button.click()
+            time.sleep(1)
+            # 提交
+            confirm_buttons = (self.browser.find_elements_by_tag_name('button'))[-1]
+            if confirm_buttons.get_attribute("textContent").find("确定") >= 0:
+                confirm_buttons.click()
+                # 确认"基本信息上报"加载完成
+                if self.wait_by_xpath('//*[@id="app"]/div/div[2]/a[2]/div[2]'):
+                    daily_done = True  # 标记已完成打卡
+                    print("打卡成功")
+                else:
+                    print("网站出现故障")
+            else:
+                print("本次打卡失败,无法点击提交按钮")
+        return daily_done
+
+    def daily_report(self):
+        # username and password
+        user = {'213192262': 'wyy13133152596.'}
+        user.update({'test': 'test'})
+        for username, password in user.items():
+            self.login(username, password)
+            self.report()
+            self.logout()
+
+
+def main():
+    test = SeuDaKa()
+    Timer(86400, test.daily_report()).start()
+
 
 if __name__ == "__main__":
-  print("This is a script for calculating NGD.")
+    main()
