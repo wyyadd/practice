1bc720d50db3b1a144b19b582ab41d180c6159c0
wyyadd <wyyadd@gmail.com>
2021-08-11 22:10:17 +0800

    Valid Parentheses

---
+++ leetcode/19/19.cc
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+			ListNode *fast = head;
+			ListNode *p_former = head;
+			ListNode *p = head;
+			for (int i = 0; i < n; i++)
+				fast = fast -> next;
+			while (fast != nullptr) {
+				if(p != head)
+					p_former = p_former -> next;
+				p = p -> next;
+				fast = fast -> next;
+			}
+			if(p != head){
+				p_former -> next = p-> next;
+				delete p;
+			}
+			else{
+				head = p -> next;
+				delete p;
+			}
+			return head;
+    }
+};
