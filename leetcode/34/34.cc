d5ec2819c940dcf6dbe206f2f4e3cb1dad1757c0
wyyadd <wyyadd@gmail.com>
2021-08-15 18:01:26 +0800

    Find First and Last Position of Element in Sorted Array

---
+++ leetcode/34/34.cc
@@ -0,0 +1,12 @@
+//using stl library
+//逃课
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+			auto [l, r] = equal_range(nums.begin(), nums.end(), target);
+      if (l == r)
+				return {-1, -1};
+			else  
+				return {(int)(l-nums.begin()), (int)(r-nums.begin()-1)};
+    }
+};
