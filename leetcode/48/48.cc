7907835a621be237e499bb1d16d167788bd36741
wyyadd <wyyadd@gmail.com>
2021-08-18 15:07:58 +0800

    Rotate Image

---
+++ leetcode/48/48.cc
@@ -0,0 +1,16 @@
+// left-up element rotate 90 whth other 3 elements
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+};
