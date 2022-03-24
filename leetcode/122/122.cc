a749dbd6792da5aaebb16082f195e53053d593c1
wyyadd <wyyadd@gmail.com>
2022-03-24 10:15:53 +0800

    leetcode

---
+++ leetcode/122/122.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    // dp[i][1] = max(dp[i-1][1], dp[i-1][0]-price[i])
+    // dp[i][0] = max(dp[i-1][0], dp[i-1][1]+price[i])
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(), vector<int>(2));
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i < prices.size(); ++i){
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i]);
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i]);
+        }
+        return max(dp[prices.size()-1][0],dp[prices.size()-1][1]);
+    }
+};
--- leetcode/sort/extendTest.cc
+++ leetcode/sort/extendTest.cc
@@ -6,6 +6,10 @@ public:
     virtual void fun(int i){
         cout << "A" << i << endl;
     }
+
+    void foo(int i){
+        cout << "fooA" << i << endl;
+    }
 };
 
 class B: public A{
@@ -13,10 +17,15 @@ public:
     void fun(int i){
         cout << "B"  << i << endl;
     }
+
+    void foo(int i){
+        cout << "fooB" << i << endl;
+    }
 };
 
 int main(){
     A *a = new B;
     a->fun(1);
     a->A::fun(1);
+    a->foo(1);
 }
